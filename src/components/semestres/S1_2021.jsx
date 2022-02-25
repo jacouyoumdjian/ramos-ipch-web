d8277c1fbf489310e8fa7d86eebf84865ca330b1
JoacoCoyu <jacouyoumdjian@uc.cl>
2022-02-25 13:11:10 -0300

    adding proposal info for ramos 2021-1

--- src/components/semestres/S1_2021.jsx
+++ src/components/semestres/S1_2021.jsx
@@ -7,24 +7,44 @@ import Collapse from '../features/Collapse';
 const S1_2021 = () => {
 
     const dataRamosS1_2021 = [
-                        ['Diseño de Elementos de Máquinas - ICM2333', 'Ingeniería Mecánica y Metalúrgica.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'],
-                        ['Diseño en Ingeniería Biomédica - IBM2123', 'Instituto de Ingeniería Biológica y Médica.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'],
-                        ['Gestión de Operaciones - ICS3213', 'Ingeniería Industrial y de Sistemas.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'],
-                        ['Ingeniería de Construcción - ICC2304', 'Ingeniería y Gestión de la Construcción.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'],
-                        ['Introducción a la Economía - ICS1513', 'Ingeniería Industrial y de Sistemas.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'],
-                        ['Investigación, Innovación y Emprendimiento - ING2030', 'Subdirección de Innovación.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'], 
-                        ['Mecánica de Fluidos - ICH1104', 'Ingeniería Hidráulica y Ambiental.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'],
-                        ['Optimización - ICS3213', 'Ingeniería Industrial y de Sistemas.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.'], 
-                        ['Recursos y Exploración Geológica - ICE2640', 'Ingeniería Estructural y Geotécnica.', 'Profesor 1.', 'Ayudante 1.', 
-                        'propuesta 1.', 'número de estudiantes.']]
+                        ['Diseño de Elementos de Máquinas - ICM2333', 'Ingeniería Mecánica y Metalúrgica.', 'Diego Celentano.', 'Isa Oyarzo.', 
+                        'El proyecto semestral del curso incorporó como temática principal las problemáticas reales de la Fundación Corpaliv. \
+                        Los estudiantes debieron abordar los desafíos de la fundación desarrollando soluciones orientadas a acoger, orientar y acompañar a niños, \
+                        niñas y jóvenes con discapacidad múltiple y a sus familias.', '44.'],
+                        ['Diseño en Ingeniería Biomédica - IBM2123', 'Instituto de Ingeniería Biológica y Médica.', 'Vicente Parot.', 'Sin ayudante.', 
+                        'Curso parcialmente Sello IPCh. Este curso capstone trabajó un proyecto brindado por iniciativa IPCh, \
+                        relacionado a enfermedad fibromialgia.', '6 estudiantes en proyecto IPCh.'],
+                        ['Gestión de Operaciones - ICS3213', 'Ingeniería Industrial y de Sistemas.', 'Alejandro MacCawley.', 'Eduards Triviño.', 
+                        'Se realizaron tareas a lo largo del semestre que contextualizaban los contenidos del curso en distintos \
+                        desafíos reales de la fundación TECHO.', '331.'],
+                        ['Ingeniería de Construcción - ICC2304', 'Ingeniería y Gestión de la Construcción.', 'Manuel Carpio.', 'Isa Oyarzo.', 
+                        'El proyecto semestral del curso consistió en que los estudiantes desarrollaran la construcción a nivel \
+                        de planos de proyectos del MINVU de viviendas sociales. Además, se realizaron charlas en las que se tuvieron a expertos en temáticas relacionadas al \
+                        a la construcción de vivienda social y el servicio público.', '69.'],
+                        ['Introducción a la Economía - ICS1513', 'Ingeniería Industrial y de Sistemas.', 'Cristobal Munoz y Emil Namur.', 
+                        'Joaquín Terreros.', 'El proyecto semestral se contextualizó en la aplicación de la economía en Políticas Públicas. \
+                        Además, se realizaon ayudantías con ejercicios relacionados a distintos desafíos ingenieriles abordables desde los \
+                        contenidos del curso.', '109.'],
+                        ['Investigación, Innovación y Emprendimiento - ING2030', 'Subdirección de Innovación.', 'Constance Fleet, \
+                        Loreto Acevedo, Alfonso Cruz, Michael Leatherbee y Patricio Lillo.', 'Isa Oyarzo.', 
+                        'La temática general del curso con la que los estudiantes deben realizar sus proyectos de innovación fue una \
+                        relacionado a la sustentabilidad: Energías Limpias. Además, para las distintas plenarias se tuvieron charlistas \
+                        con expertos en temáticas relacionadas al servicio público, emprendimiento social y el mismo tema del curso.', 
+                        '518.'],
+                        ['Mecánica de Fluidos - ICH1104', 'Ingeniería Hidráulica y Ambiental.', 'Cristián Escauriaza, \
+                        Wernher Brevis y Karina Soto.', 'Javiera Boada e Isa Oyarzo.', 
+                        'Se realizaron charlas en las que se tuvieron a expertos en temáticas relacionadas al \
+                        riesgo con eventos catastróficos, hidrometeorología y agua potable. Además, se realizaron \
+                        ayudantías en las que se desarrollaron ejercicios contextualizados en un problema real de \
+                        gran impacto a personas, sociedad y/o el ecosistema en su conjunto.', '518.'],
+                        ['Optimización - ICS1113', 'Ingeniería Industrial y de Sistemas.', 'Gustavo Angulo.', 'Martín Cadagan.', 
+                        'En el proyecto semestral del curso los estudiantes trabajaron con desafíos reales relacionados a las \
+                        áreas de urgencias y gestión de UC Christus. Asimismo, se invitaron a charlistas que entregaron su \
+                        testimonio sobre cómo aplicar la optimización en un caso real de gran impacto a personas.', '645.'], 
+                        ['Recursos y Exploración Geológica - ICE2640', 'Ingeniería Estructural y Geotécnica.', 'Carlos Marquardt.', 'Isa Oyarzo.', 
+                        'Se efectuaron dos salidas a terreno en una minera local de Santiago en la que se realizó un trabajo \
+                        de exploración geológica y se entregó el material de estudio a la comunidad cercana a la minera. \
+                        Además, se realizaron charlas en las que se tuvieron a expertos en minería sustentable.', '6.']]
 
     return (
         <div className='view-container'>
--- src/components/semestres/S2_2020.jsx
+++ src/components/semestres/S2_2020.jsx
@@ -8,15 +8,15 @@ const S2_2020 = () => {
 
     const dataRamosS2_2020 = [
                         ['Gestión de Operaciones en la Construcción - ICC3244', 'Ingeniería y Gestión de la Construcción.', 'Miguel Mora.', 'Leonardo Caamaño.', 'Se invitaron charlistas que entregaron su testimonio \
-                        sobre cómo aplicar la optimización en un caso real de gran impacto a las personas. Se realizaron actividades en clase en que durante los \
-                        primeros 30 minutos de la clase se daba un tiempo para investigar acerca de un tema que proponía el ayudante \
-                        (atinente a los pilares del sello), y luego este se discutía entre los estudiantes. \
+                        sobre cómo aplicar la optimización en la construcción en un caso real de gran impacto a las personas. Se realizaron actividades en clase en que durante los \
+                        primeros 30 minutos se daba un tiempo para investigar acerca de un tema que proponía el ayudante \
+                        (atingente a los pilares del sello), y luego este se discutía entre los estudiantes. \
                         Se les entregó lecturas a los estudiantes para que complementarán su formación en el curso, y fueran capaces de ver la aplicación \
                         de los contenidos en casos reales. Se realizó una interrogación que evaluó los conocimientos y \
                         competencias de los estudiantes, contextualizada en un caso real.', '38.'],
                         ['Gestión de Recursos Hídricos - ICH3710', 'Ingeniería Hidráulica y Ambiental.', 'Sebastián Vicuña.', 'Vicente Jander.', 
                         'En las tareas se incorporó una pregunta de investigación bibliográfica acerca de un caso real \
-                        que se vinculara con la aplicación de los contenidos del curso, y fomente la reflexión personal \
+                        que se vinculara con la aplicación de los contenidos del curso, y fomentase la reflexión personal \
                         del estudiante. En las ayudantías se realizaron ejercicios contextualizados en un problema real de gran impacto a personas, \
                         sociedad y/o el ecosistema en su conjunto. Se destinaba una sección de la cátedra para tener discusiones abiertas sobre \
                         abiertamente las respuestas de cada unos de los estudiantes a la pregunta de investigación bibliográfica.',
@@ -26,15 +26,15 @@ const S2_2020 = () => {
                         entregado a instituciones de educación secundaria. En las ayudantías se realizaron ejercicios contextualizados en un problema real de \
                         gran impacto a personas, sociedad y/o el ecosistema en su conjunto.', '577.'],
                         ['Optimización - ICS1113', 'Ingeniería Industrial y de Sistemas.', 'Gustavo Angulo.', 'Martin Cadagan, Camila Toro.', 'En el proyecto semestral se incorporaron \
-                        temáticas de problemáticas reales de la Municipalidad de Las Condes. En las ayudantías transversales se realizaron ejercicios \
+                        como temáticas problemáticas reales de optimización de la Municipalidad de Las Condes. En las ayudantías transversales se realizaron ejercicios \
                         contextualizados en un problema real de gran impacto a personas, sociedad y/o el ecosistema en su conjunto. \
                         Se invitaron a charlistas que entregaron su testimonio sobre cómo aplicar la optimización en un caso real de gran impacto a personas.',
                         '586.'],
                         ['Sustentabilidad en la Construcción - ICC3464', 'Ingeniería y Gestión de la Construcción.', 'Manuel Carpio.', 'Leonardo Caamaño.', 
                         'Se realizaron distintos controles acerca de las lecturas complementarias y casos reales relacionados \
-                        al curso que fomentaban el pensamiento crítico en los estudiantes. Se entregaron lecturas a \
-                        los estudiantes para complementar su formación en el curso, y sean capaces de ver la aplicación de los contenidos \
-                        en casos reales. Se invitaron a charlistas que entregaron su testimonio sobre cómo aplicar la aplicar la sustentabilidad \
+                        a los contenidos del curso que fomentaban el pensamiento crítico en los estudiantes. Se entregaron lecturas a \
+                        los estudiantes para complementar su formación en el curso, y apreciaran la aplicación de los contenidos \
+                        en casos reales. Se invitaron a charlistas que entregaron su testimonio sobre cómo aplicar la sustentabilidad \
                         en la construcción en un caso real de gran impacto a personas.', '37.'],
                         ['Taller de Evaluación, Tratamiento y Remediación Ambiental - ICH2394', 'Ingeniería Hidráulica y Ambiental.', 'María Molinos.', 
                         'Daniela Madrazo, Angel Perez (contacto Puentes UC).', 'Se colaboró en la comunicación y coordinación con Puentes UC \
